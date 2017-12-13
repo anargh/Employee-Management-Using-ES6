6876c2b7e56a7d488d1c25d2c22837603c6c1b67
Anargh V <anargh7595@gmail.com>
2017-12-13 20:06:57 +0530

    Updates

--- js/json_table.js
+++ js/json_table.js
@@ -24,6 +24,7 @@ const tableStructure = {
 }
 
 let employeeJson = { };
+
 //JSON DATA CLASS
 class JsonData {
   constructor(url,requestMethod) {
@@ -32,14 +33,18 @@ class JsonData {
   }
 
   requestJson(url,requestMethod) {
+    console.log("Requesting...");
     let jsonRequest = new XMLHttpRequest();
     jsonRequest.open(requestMethod, url);
     jsonRequest.responseType = this.responseType;
     jsonRequest.send();
     jsonRequest.onreadystatechange = () => {
       try {
-        if(jsonRequest.status == 200)
+        if(jsonRequest.status == 200) {
           employeeJson = jsonRequest.response;
+          document.dispatchEvent(responseRecieved);
+          console.log("Response Recieved");
+        }
         else
           throw new RequestError("There was a problem retrieving the requested resource.");
       }
@@ -50,39 +55,78 @@ class JsonData {
   }
 }
 //new JsonTabulated(1) for initializing table
-class JsonTabulated extends JsonData {
+class JsonTabulated {
   constructor() {
-    this.printRecords(tableStructure[currentPage]);
+    this.printRecords(tableStructure.currentPage);
   }
 
   initTable(pages) {
+    console.log("Initializing");
     if(pages > 1) {
       let prevButton = document.createElement("button");
       prevButton.id = "prevButton";
       prevButton.innerText = "Prev";
       prevButton.className = "emp-button";
       buttonContainer.appendChild(prevButton);
+      const self = this;
+      prevButton.addEventListener("click", function(event) {
+        self.scrollRecords(event.target);
+      });
+      for(let loopIndex = 1; loopIndex <= tableStructure[totalPages]; loopIndex++) {
+        let pageNumberButton = document.createElement("button");
+        pageNumberButton.innerText = loopIndex;
+        pageNumberButton.className = "emp-button";
+        buttonContainer.appendChild(pageNumberButton);
+        pageNumberButton.addEventListener("click", function(event) {
+          currentPage = Number(event.target.innerText);
+          self.printRecords(currentPage);
+        });
+      }
+      let nextButton = document.createElement("button");
+      nextButton.id = "nextButton";
+      nextButton.innerText = "Prev";
+      nextButton.className = "emp-button";
+      buttonContainer.appendChild(nextButton);
+      nextButton.addEventListener("click", function(event) {
+        self.scrollRecords(event.target);
+      });
     }
   }
   printRecords(page) {
+    tableStructure.currentPage = page;
     let recordCount = Object.keys(employeeJson).length;
     tableStructure.totalPages = Math.ceil(recordCount / tableStructure.recordPerPage);
-    if(tableStructure[isTableInit] == false)
+    if(tableStructure.isTableInit == false)
       this.initTable(tableStructure.totalPages);
-    let startIndex = ((currentPage - 1) * recordPerPage) + 1,
-        endIndex = startIndex + recordPerPage;
+    let startIndex = ((tableStructure.currentPage - 1) * tableStructure.recordPerPage) + 1,
+        endIndex = startIndex + tableStructure.recordPerPage;
     this.removeRows(tableStructure[tableElement].rows.length - 1);
     singlePageRecords = employeeJson.slice(startIndex, endIndex);
+  }
+  scrollRecords(button) {
+    direction = button.innerText.toLowerCase();                                   // Direction - forward (next) or backward (prev)
+    if((direction == "next") && (tableStructure.currentPage < tableStructure.totalPages)) {
+      ++currentPage;
+      printRecords(tableStructure.currentPage);
+    }
+    if((direction == "prev") && (tableStructure.currentPage > 1)) {
+      --currentPage;
+      printRecords(tableStructure.currentPage);
+    }
   }
   removeRows(rowCount) {
     for(; rowCount > 0; --rowCount)
       tableStructure[tableElement].deleteRow(rowCount);
   }
 }
+console.log("Here");
+document.addEventListener("DOMContentLoaded", () => new JsonData("http://127.0.0.1/emp_es6/employee.json","POST"));
+let responseRecieved = new CustomEvent("ResponseRecieved");
+document.addEventListener("ResponseRecieved", () => new JsonTabulated);
 
 function showError(name, message) {
-  tableElement.style.display = 'none';
-  var errorMessage = document.getElementById("tableErrorBox");
+  tableStructure.tableElement.style.display = 'none';
+  let errorMessage = document.getElementById("tableErrorBox");
   errorMessage.style.display = 'block';
-  errorMessage.innerHTML = "ERROR: <strong>" + name + "</strong>: " + message + "<br /> Returned Response: " +jsonRequest.status+": "+jsonRequest.statusText;
+  errorMessage.innerHTML = "ERROR: <strong>" + name + "</strong>: " + message + "<br /> Returned Response: ";
 }
